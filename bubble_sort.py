00831e133e658dc084b410463738dba779ac24a8
Shivam Singh <stdnt.shivam@gmail.com>
2021-03-14 20:14:13 +0530

    Fixed Bug

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,4 @@
 import time
-from main import stop
 
 
 def bubble_sort(data, draw_data, speed):
@@ -8,8 +7,6 @@ def bubble_sort(data, draw_data, speed):
         store = len(data) - 1 - n
         for j in range(store):
             if data[j] > data[j + 1]:
-                if stop:
-                    return
                 data[j], data[j + 1] = data[j + 1], data[j]
                 draw_data(data, optional_color='red', digit=j, end=store)
                 time.sleep(speed)
